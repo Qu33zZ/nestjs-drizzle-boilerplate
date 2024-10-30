56c64d724ee74724272e0de6284353951a258161
Ruslan <66295121+BlueRexPY@users.noreply.github.com>
2024-10-30 01:26:53 +0100

    Update cars.module.ts

--- src/apps/api/modules/cars/cars.module.ts
+++ src/apps/api/modules/cars/cars.module.ts
@@ -2,5 +2,5 @@ import { Module } from '@nestjs/common';
 import { CarsController } from './cars.controller';
 import { CarsModule as AppCarModule } from '@application/cars/cars.module';
 
-@Module({ controllers: [CarsController], imports: [AppCarModule] })
+@Module({ controllers: [CarsControllerV1], imports: [AppCarModule] })
 export class CarsModule {}
